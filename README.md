a02963b69f4adea32a45cbbc6fc240445a3cc98c
mbetts7 <mbetts7@gmail.com>
2014-02-03 10:20:10 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Week 1 roadmap:
+- set up models
+- sorting date/amnt paid/
+- I owe $ / Due to me $
+- Dropdown from contacts
+- emailing
+- stripe integration
+- charting
+- upload to heroku
+
+Future roadmap:
+- ability to split ious
+- temporary groups to split ious
+- autofill from contacts
+- file upload for checks / docs / invoices...tie to ious
+- make reoccurring
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DebtsWithFriends::Application.config.secret_key_base = 'aa13f3ed2bff525d8d69f101f9acd33004a68bb16102012a1d8b9a1de37b349aedb35408190255feac85bef43c66a1b503cccff2855a1623a667241ce98ad964'
