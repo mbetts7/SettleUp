01fdb0a652f484bbedc6d0c17e696874becd5c84
mbetts7 <mbetts7@gmail.com>
2014-02-04 12:54:19 -0800

    can create ious and show them

--- app/controllers/ious_controller.rb
+++ app/controllers/ious_controller.rb
@@ -2,27 +2,37 @@ class IousController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
   def index
-    # user = User.find_by_id(:user.id)
     @ious = current_user.ious
-    # @ious = Iou.all
   end
 
   def new
+    @iou = Iou.new
   end
 
   def create
+    new_iou = params.require(:iou).permit(:balance, :name, :memo, :status, :due_date)
+    @iou = Iou.create(new_iou)
+    render :show
   end
 
   def show
+    @iou = Iou.find(params[:id])
   end
 
   def edit
+    @iou = Iou.find(params[:id])
   end
 
   def update
+    @iou = Iou.find(params[:id])
+    @iou.update_attributes(params[:iou])
+    render :show
   end
 
   def destory
+    iou = Iou.find(params[:id])
+    iou.delete
+    redirect_to(ious_path)
   end
   
 end
