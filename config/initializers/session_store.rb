954083e244279b834a584f6d17fbbb9320375526
mbetts7 <mbetts7@gmail.com>
2014-02-03 18:42:45 -0800

    deleted auto gen scss files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SettleUp::Application.config.session_store :cookie_store, key: '_DebtsWithFriends_session'
+SettleUp::Application.config.session_store :cookie_store, key: '_SettleUp_session'
