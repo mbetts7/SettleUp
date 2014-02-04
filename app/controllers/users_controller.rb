41e8b1bd8390ac661b40a067020491684063af4f
mbetts7 <mbetts7@gmail.com>
2014-02-03 16:15:01 -0800

    printed out users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
 end
