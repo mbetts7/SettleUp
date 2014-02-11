aae58e2fae748e23120efd01d5da1b8edfa71648
mbetts7 <mbetts7@gmail.com>
2014-02-10 20:37:38 -0800

    new relic updated

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SettleUp::Application.config.secret_key_base = 'aa13f3ed2bff525d8d69f101f9acd33004a68bb16102012a1d8b9a1de37b349aedb35408190255feac85bef43c66a1b503cccff2855a1623a667241ce98ad964'
+SettleUp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
